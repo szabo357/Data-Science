59b63f677c699e134e9b5709b3b8ed0ef6c5856c
szabo357 <szabo357@gmail.com>
2024-03-10 22:24:26 -0600

    update code

---
+++ looping_data_structures.py
@@ -0,0 +1,12 @@
+
+# Example 1
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin',
+          'norway':'oslo', 'italy':'rome', 'poland':'warsaw', 'austria':'vienna' }
+          
+# Iterate over europe dictionary
+for country, capital in europe.items() :
+    print("the capital of " + country + " is " + capital)
+
+
+np_height = [74, 74, 72, 73, 74, 75, 75, 73]
--- while_loop.py
+++ while_loop.py
@@ -1,3 +1,4 @@
+# Exercises using the while loop in Python.
 # While loop exercise 1
 # Initialize offset
 offset = 8
@@ -8,3 +9,16 @@ while offset != 0 :
     offset = offset - 1
     print(offset)
 
+
+# Example 2
+# Initialize offset
+offset = -6
+
+# Code the while loop
+while offset != 0 :
+    print("correcting...")
+    if offset > 0 :
+      offset = offset - 1
+    else : 
+      offset = offset + 1    
+    print(offset)
